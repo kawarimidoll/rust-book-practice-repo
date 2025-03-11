a163ab9f4c74a5b868a6f6aff49d4a9af4bd8beb
kawarimidoll <kawarimidoll+git@gmail.com>
2025-03-11 10:41:56 +0900

    feat: add health check

--- rusty-book-manager/src/main.rs
+++ rusty-book-manager/src/main.rs
@@ -1,18 +1,31 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
 async fn hello_world() -> &'static str {
     "Hello, World!"
 }
 
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let status = health_check().await;
+    assert_eq!(status, StatusCode::OK);
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     println!("Hello, world!");
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new()
+        .route("/hello", get(hello_world))
+        .route("/health", get(health_check));
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(&addr).await.unwrap();
+    let listener = TcpListener::bind(&addr).await?;
     println!("Server running on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
